58af373b62c9df7fe2c8d190e8fedd16178236c4
Ali <alibili@fifi.com>
2025-10-23 03:53:14 -0700

    correct for dokploy deploy1

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 
 def home_page_view(request):
-    return HttpResponse ('Hello Django Worker')
+    return HttpResponse ('Welcome to  **LiLi** django web site ')
 
 # Create your views here.
